ae6ada0286c64ee9b5eb10c5ed7862cb79588178
Benjamin Walkenhorst <krylon@posteo.de>
2026-05-14 09:51:33 +0200

    main - clean references to previous project

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
-// /home/krylon/go/src/github.com/blicero/newsroom/main.go
+// /home/krylon/go/src/github.com/blicero/vorleser/main.go
 // -*- mode: go; coding: utf-8; -*-
 // Created on 09. 03. 2026 by Benjamin Walkenhorst
 // (c) 2026 Benjamin Walkenhorst
-// Time-stamp: <2026-05-13 13:39:30 krylon>
+// Time-stamp: <2026-05-14 09:49:16 krylon>
 
 package main
 
@@ -11,21 +11,16 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
-	"runtime"
 	"runtime/pprof"
 	"syscall"
 	"time"
 
-	"github.com/blicero/newsroom/common"
-	"github.com/blicero/newsroom/engine"
-	"github.com/blicero/newsroom/web"
+	"github.com/blicero/vorleser/common"
 )
 
 func main() {
 	var (
 		err     error
-		eng     *engine.Engine
-		srv     *web.Server
 		ticker  *time.Ticker
 		addr    string
 		profOut string
@@ -37,7 +32,6 @@ func main() {
 		common.Version,
 		common.BuildStamp.Format(common.TimestampFormat))
 
-	flag.StringVar(&addr, "addr", fmt.Sprintf("[::1]:%d", common.WebPort), "The IP address for the web UI to listen on")
 	flag.StringVar(&profOut, "prof", "", "if non-empty, write profiling information to the named file")
 	flag.Parse()
 
@@ -69,23 +63,6 @@ func main() {
 		defer pprof.StopCPUProfile()
 	}
 
-	if eng, err = engine.Create(runtime.NumCPU()); err != nil {
-		fmt.Fprintf(
-			os.Stderr,
-			"Failed to create Engine: %s\n",
-			err.Error())
-		os.Exit(1)
-	} else if srv, err = web.Create(addr, eng); err != nil {
-		fmt.Fprintf(
-			os.Stderr,
-			"Failed to create web server: %s\n",
-			err.Error())
-		os.Exit(1)
-	}
-
-	eng.Start()
-	go srv.Run()
-
 	ticker = time.NewTicker(common.ActiveTimeout)
 	defer ticker.Stop()
 
@@ -94,15 +71,11 @@ func main() {
 	for {
 		select {
 		case <-ticker.C:
-			if !eng.IsActive() {
-				return
-			}
 		case s := <-sigQ:
 			fmt.Fprintf(
 				os.Stderr,
 				"Caught signal: %s\n",
 				s)
-			eng.Stop()
 			return
 		}
 	}
