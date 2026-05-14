140417d5a597c3738895928747a25e3e6ef73754
Benjamin Walkenhorst <krylon@posteo.de>
2026-05-14 09:43:10 +0200

    model - continue fleshing out the data model

--- model/model.go
+++ model/model.go
@@ -2,17 +2,62 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 13. 05. 2026 by Benjamin Walkenhorst
 // (c) 2026 Benjamin Walkenhorst
-// Time-stamp: <2026-05-13 15:48:13 krylon>
+// Time-stamp: <2026-05-14 09:42:05 krylon>
 
 package model
 
-type File struct {
-	ID   int64
-	Path string
+import (
+	"net/url"
+	"path"
+	"path/filepath"
+	"time"
+)
+
+// Book is a ... well, a book. Really.
+type Book struct {
+	ID          int64
+	CurrentFile int64
+	Title       string
+	Author      string
+	URL         *url.URL
 }
 
-type Book struct {
-	ID     int64
-	Title  string
-	Author string
+// Folder is a directory containing Files.
+type Folder struct {
+	ID       int64
+	Path     string
+	LastScan time.Time
 }
+
+// File is an audio file.
+type File struct {
+	ID         int64
+	BookID     int64
+	FolderID   int64
+	Path       string
+	Title      string
+	Ord        []int64
+	Position   int64
+	LastPlayed time.Time
+}
+
+// DisplayTitle returns the File's title, or its filename, if no title is known.
+func (f *File) DisplayTitle() string {
+	if f.Title != "" {
+		return f.Title
+	}
+
+	return path.Base(f.Path)
+} // func (f *File) DisplayTitle() string
+
+// GetParentFolder returns the name of the Folder the file lives in,
+// i.e. basename(dirname(path))
+func (f *File) GetParentFolder() string {
+	return filepath.Base(filepath.Dir(f.Path))
+} // func (f *File) GetParentFolder() string
+
+// PathURL returns the File's path as a file:// URL.
+// Intended for use with DBus interfaces
+func (f *File) PathURL() string {
+	return "file://" + url.PathEscape(f.Path)
+} // func (f *File) PathURL() string
